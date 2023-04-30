0abd2a22fd2a026c3b649471505e009a736abe96
Nikki010101 <111778589+Nikki010101@users.noreply.github.com>
2023-04-30 15:03:25 -0500

    added

---
+++ src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+*
+* @author Nikhitha Kasuvojula s556393
+*/
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.example.DMS.repository;
+/**
+*
+* @author Nikhitha Kasuvojula s556393
+*/
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
